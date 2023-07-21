7297432d125d8b57d3e589818d2b73383c2b33d2
Barbara Queval <115092715+QuevalBarbara@users.noreply.github.com>
2023-07-21 19:49:04 +0200

    Update RestaurantList.jsx

--- frontend/components/RestaurantList.jsx
+++ frontend/components/RestaurantList.jsx
@@ -32,7 +32,7 @@ function RestaurantCard({ data }) {
           className="w-full rounded-2xl"
           height={300}
           width={300}
-          src={`${process.env.STRAPI_URL || "https://strapi-b4ct.onrender.com"}${
+          src={`${process.env.STRAPI_URL || "https://strapi-b4ct.onrender.com?populate=*"}${
             data.attributes.image.data[0].attributes.url
           }`}
           alt=""
