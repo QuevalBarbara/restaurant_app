85a948504dc990349b3dd6d590ad98857268a470
Barbara Queval <115092715+QuevalBarbara@users.noreply.github.com>
2023-07-22 12:16:58 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,15 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "strapi-b4ct.onrender.com",
-          pathname: "/uploads/**",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "strapi-b4ct.onrender.com",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
